001672e18e1c19c66817e9ee04449dcb66542ada
jaysaavedra18 <jasonsaavedra@yahoo.com>
2023-08-09 20:40:56 -0400

    added more confirmation for input, printing más information, and helper functions for data

--- create_playlist.py
+++ create_playlist.py
@@ -43,14 +43,43 @@ def get_audio_files(directory):
     return [f for f in os.listdir(directory) if f.endswith(".mp3") or f.endswith(".wav")]
 
 
+def get_audio_info(audio_files):
+    total_size = sum(os.path.getsize(file) for file in audio_files)
+    total_length = sum(AudioSegment.from_file(
+        file).duration_seconds for file in audio_files)
+    return total_size, total_length
+
+
+def format_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
 def select_audio_files_with_dialog():
-    root = Tk()
-    root.withdraw()  # Hide the main window
-    selected_files = filedialog.askopenfilenames(
-        initialdir=AUDIO_DIRECTORY, title="Select Audio Files", filetypes=(("Audio Files", "*.mp3 *.wav"),)
-    )
-    # Extract only file names
-    return [os.path.basename(file) for file in selected_files]
+    while True:
+        root = Tk()
+        root.withdraw()  # Hide the main window
+        selected_files = filedialog.askopenfilenames(
+            initialdir=AUDIO_DIRECTORY, title="Select Audio Files", filetypes=(("Audio Files", "*.mp3 *.wav"),)
+        )
+
+        total_size, total_length = get_audio_info(selected_files)
+        print(f"Number of files: {len(selected_files)}")
+        print(f"Total size: {total_size / (1024 * 1024):.2f} MB")
+        print(f"Total length: {format_time(total_length)}")
+
+        while True:
+            choice = input(
+                "Are you satisfied with the length of the audio? (y/n): ").lower()
+            if choice == 'y':
+                root.destroy()  # Close the file dialog window
+                return [os.path.basename(file) for file in selected_files]
+            elif choice == 'n':
+                break  # Exit the input loop and re-enter the file explorer.
+            else:
+                print("Invalid choice. Please enter 'y' or 'n'.")
 
 
 def select_random_files(audio_files, num_files):
@@ -84,11 +113,6 @@ def generate_output_references(selected_files, text_blocks):
                 total_duration += duration_seconds
         except ValueError:
             print(f"Skipped: {file} (Not in number format)")
-            position = f"{int(total_duration // 60):02}:{int(total_duration % 60):02}"
-            filename_without_extension = os.path.splitext(file)[0]
-            output_references.append(
-                f"{position} {filename_without_extension} from HookSounds.com")
-            total_duration += duration_seconds
 
     return output_references
 
