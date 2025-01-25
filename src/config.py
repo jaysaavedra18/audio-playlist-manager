2908b193d4e0f3a7893f77d0f7d5867fdabb2605
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-25 10:52:25 -1000

    updated some path names and added references.txt to data directory

--- src/config.py
+++ src/config.py
@@ -12,12 +12,12 @@ DATE_STRING = today_date.strftime("%Y-%m-%d")
 
 # Define directory paths
 BASE_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Get parent dir of src folder
-AUDIO_DIRECTORY = os.path.join(BASE_DIRECTORY, "audio")
+AUDIO_DIRECTORY = "/Users/saavedj/Downloads/music"
 DATA_DIRECTORY = os.path.join(BASE_DIRECTORY, "data")
 ASSETS_DIRECTORY = os.path.join(BASE_DIRECTORY, "yt_assets")
 ARCHIVES_DIRECTORY = os.path.join(ASSETS_DIRECTORY, f"{DATE_STRING}")
-DOWNLOADS_DIRECTORY = os.path.join(AUDIO_DIRECTORY, "downloads")
-LIBRARY_DIRECTORY = os.path.join(AUDIO_DIRECTORY, "library")
+DOWNLOADS_DIRECTORY = os.path.join(AUDIO_DIRECTORY, "misc")
+LIBRARY_DIRECTORY = os.path.join(AUDIO_DIRECTORY, "lofi")
 
 # Define data file paths
 REFERENCES_PATH = os.path.join(DATA_DIRECTORY, "references.txt")
