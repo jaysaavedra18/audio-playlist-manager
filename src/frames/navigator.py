58482184179db010592a3209a1983fc4613f91ae
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-26 10:22:25 -1000

    creating a navigator to handle circular imports

---
+++ src/frames/navigator.py
@@ -0,0 +1,16 @@
+def navigagte_to(frame, app_context):
+    if frame == "main_menu":
+        from main_menu import MainMenuFrame
+        return MainMenuFrame(app_context)
+    elif frame == "playlist_creator":
+        from playlist_creator import PlaylistCreatorFrame
+        return PlaylistCreatorFrame(app_context)
+    elif frame == "song_library":
+        from song_library import SongLibraryFrame
+        return SongLibraryFrame(app_context)
+    elif frame == "edit_tags":
+        from edit_tags import EditTagsFrame
+        return EditTagsFrame(app_context)
+    elif frame == "create_by":
+        from create_by import CreateByFrame
+        return CreateByFrame(app_context)
