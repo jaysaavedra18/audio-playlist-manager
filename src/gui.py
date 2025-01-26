170073324aa1d8758777c08ae32781dcfae13102
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-26 09:58:16 -1000

    moving frames into separate directory for modularity

--- src/gui.py
+++ src/gui.py
@@ -60,22 +60,6 @@ class Application(tk.Tk):
         self.current_frame = new_frame
 
 
-class MainMenuFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        label = tk.Label(self, text="Main Menu")
-        label.pack()
-
-        playlist_button = tk.Button(
-            self, text="Playlist Creator", command=lambda: master.show_frame(PlaylistCreatorFrame))
-        playlist_button.pack()
-
-        option2_button = tk.Button(
-            self, text="Song Library", command=lambda: master.show_frame(SongLibraryFrame))
-        option2_button.pack()
-
-
 class PlaylistCreatorFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
