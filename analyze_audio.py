09caac3fde246b515472117591853aad15e9ce2a
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-26 00:52:56 -1000

    working on spectogram to show frequeny

--- analyze_audio.py
+++ analyze_audio.py
@@ -32,6 +32,12 @@ def plot_waveform(y: np.ndarray, sr: int) -> None:
     plt.ylabel("Amplitude")
     plt.show()
 
+def plot_spectogram(y: np.ndarray, sr: int) -> None:
+    """Plot the spectrogram of an audio signal."""
+    S = librosa.feature.melspectrogram(y=y, sr=sr)
+    plt.figure(figsize=(12, 4))
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), sr=sr, y_axis='mel', x_axis='time')
+
 
 audio_results = analyze_audio("/Users/saavedj/Downloads/music/misc/16.mp3")
 plot_waveform(audio_results[0], audio_results[1])
