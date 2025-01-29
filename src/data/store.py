5e331db07b74b196b3425d4d5e23ed2ec2319d3e
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-28 20:46:18 -1000

    store supports update, add, get, and init. everything needed for success

--- src/data/store.py
+++ src/data/store.py
@@ -11,3 +11,26 @@ class DataStore:
     def __init__(self) -> None:
         """Initialize the data store with the specified database."""
         self.db = read_json(LIBRARY_DATA_PATH, AudioFile)
+
+    def get(self, song_name: str) -> AudioFile:
+        """Get the audio file with the specified song name."""
+        for audio_file in self.db:
+            if audio_file.song_name == song_name:
+                return audio_file
+        message = f"Audio file with song name '{song_name}' not found."
+        raise ValueError(message)
+
+    def add(self, audio_file: AudioFile) -> None:
+        """Add the audio file to the data store."""
+        self.db.append(audio_file)
+        write_json(LIBRARY_DATA_PATH, self.db)
+
+    def update(self, audio_file: AudioFile) -> None:
+        """Update the audio file in the data store."""
+        for i, file in enumerate(self.db):
+            if file.song_name == audio_file.song_name:
+                self.db[i] = audio_file
+                write_json(LIBRARY_DATA_PATH, self.db)
+                return
+        message = f"Audio file with song name '{audio_file.song_name}' not found."
+        raise ValueError(message)
