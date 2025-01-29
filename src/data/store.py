db8323c1746c0479e1e5f408fb6e33ab6365bc13
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-28 20:39:42 -1000

    working on global storage

--- src/data/store.py
+++ src/data/store.py
@@ -1,14 +1,13 @@
+# ruff: noqa
+
+from utils.files import read_json, write_json
+from config import LIBRARY_DATA_PATH
+from models.audio_file import AudioFile
+
+
 class DataStore:
     """A simple key-value store that stores data in memory."""
 
-    def __init__(self, db: dict) -> None:
+    def __init__(self) -> None:
         """Initialize the data store with the specified database."""
-        self.db = db
-
-    def get(self, key: str) -> str:
-        """Retrieve the value associated with the specified key from the data store."""
-        return self.db.get(key)
-
-    def set(self, key: str, value: str) -> None:
-        """Store the specified key-value pair in the data store."""
-        self.db.set(key, value)
+        self.db = read_json(LIBRARY_DATA_PATH, AudioFile)
