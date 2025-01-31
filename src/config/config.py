fe0c7a1ab4356c1ce538cb87723da6736f38bd1c
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-30 15:50:48 -1000

    changes

---
+++ src/config/config.py
@@ -0,0 +1,13 @@
+# ruff: noqa
+
+
+from .env_config import AUDIO_DIRECTORY
+from .date_config import DATE_STRING
+from .path_config import (
+    BASE_DIRECTORY,
+    DATA_DIRECTORY,
+    DAILY_PLAYLIST_DIRECTORY,
+    DOWNLOADS_DIRECTORY,
+    LIBRARY_DIRECTORY,
+    LIBRARY_DATA_PATH,
+)
